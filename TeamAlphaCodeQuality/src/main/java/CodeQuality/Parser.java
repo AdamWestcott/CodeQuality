d397a91a98086c5ec0565fec53e27eecb28a09b3
Adam Westcott <k1746252@kingston.ac.uk>
2020-03-03 16:58:44 +0000

    Added a method which splits code into the parser

--- TeamAlphaCodeQuality/src/main/java/CodeQuality/Parser.java
+++ TeamAlphaCodeQuality/src/main/java/CodeQuality/Parser.java
@@ -5,10 +5,35 @@
  */
 package CodeQuality;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author k1746252
  */
-public class Parser {
+public class Parser {	 	 
+    
+  public static String[] parseSplit(String data)	 	       	    	      	  	     	  	
+  {	 	       	    	      	  	     	  	
+    data = data+" ";	 	       	    	      	  	     	  	
+    ArrayList<Integer> list = new ArrayList<Integer>();	 	       	    	      	  	     	  	
+    ArrayList<String> list2 = new ArrayList<String>();	 	       	    	      	  	     	  	
+   for (int i = data.indexOf(" "); i!= -1; i=data.indexOf(" ",i+1))	 	       	    	      	  	     	  	
+   {	 	       	    	      	  	     	  	
+   int a = i;	 	       	    	      	  	     	  	
+   list.add(a);	 	       	    	      	  	     	  	
+   }	 	       	    	      	  	     	  	 	       	    	      	  	     	  	
+    Integer [] space = new Integer[list.size()];	 	       	    	      	  	     	  	
+    list.toArray(space);	 	       	    	      	  	     	  	
+    int a =0;	 	       	    	      	  	     	  	
+    for (int i = 0; i<space.length; i++)	 	       	    	      	  	     	  	
+         {	 	       	    	      	  	     	  	
+           list2.add(data.substring(a,  space[i]));	 	       	    	      	  	     	  	
+           a=space[i]+1;	 	       	    	      	  	     	  	
+         }	 	       	    	      	  	     	  		       	    	      	  	     	  	
+     String [] pars = new String[list2.size()];	 	       	    	      	  	     	  	
+    list2.toArray(pars);	 	       	    	      	  	     	  	 	       	    	      	  	     	  	
+        return pars;	 	       	    	      	  	     	  	
+  }
     
 }
